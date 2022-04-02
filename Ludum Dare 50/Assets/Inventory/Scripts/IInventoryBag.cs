72bfff3ac5c9b0a247651b914f36dffa092e0e4f
Pierre-Alain Vigeant <pierre-alain.vigeant@hotmail.com>
2022-04-02 16:31:05 -0400

    Gestion basique de l'inventaire

---
+++ Ludum Dare 50/Assets/Inventory/Scripts/IInventoryBag.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+
+public interface IInventoryBag
+{
+    /// <summary>
+    /// Retourne l'item choisi sans le faire disparaître de l'inventaire.
+    /// </summary>
+    GameObject Peek();
+
+    /// <summary>
+    /// Retourne l'item choisi et le retire de l'inventaire.
+    /// </summary>
+    /// <returns></returns>
+    GameObject Use();
+
+    event ItemSelectedHandler ItemSelected;
+}
+
+public delegate void ItemSelectedHandler(object sender, ItemSelectedEventArgs e);
+
+public class ItemSelectedEventArgs : EventArgs
+{
+    public ItemSelectedEventArgs(GameObject o)
+    {
+        Item = o;
+    }
+
+    public GameObject Item { get; private set; }
+}

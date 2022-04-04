1560703b33fe622d6f2ca8fa8b549b8148a5a92c
Mister Bamboo <nicolascnaud1989@gmail.com>
2022-04-04 19:33:19 -0400

    feat: Flashing exclamation mark when critical

--- Ludum Dare 50/Assets/HUD/FloodLevelHud/FloodLevelHudController.cs
+++ Ludum Dare 50/Assets/HUD/FloodLevelHud/FloodLevelHudController.cs
@@ -1,6 +1,7 @@
 using Assets.GameManagement;
 using Assets.Tower;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FloodLevelHudController : MonoBehaviour
 {
@@ -8,12 +9,15 @@ public class FloodLevelHudController : MonoBehaviour
     [SerializeField] RectTransform towerRect;
     [SerializeField] RectTransform waterRect;
     [SerializeField] RectTransform playerRect;
+    [SerializeField] Image exclamationMarkImage;
 
     [SerializeField] float minHeight = 4;
 
     private GameObject player;
     private IFloodLevel floodLevel;
     private ITowerHeightDetector towerHeightDetector;
+    private bool isCritical;
+    private float isCriticalBlinkTime;
 
     private void Start()
     {
@@ -26,25 +30,35 @@ public class FloodLevelHudController : MonoBehaviour
     {
         var height = GetMaxGameHeight();
         UpdateTowerRectUIHeight(height);
-        UpdateWaterRectUIHeight2(height);
+        UpdateWaterRectUIHeight(height);
         UpdatePlayerUIHeight(height);
+        UpdateIsCritical(height);
+    }
+
+    private void Update()
+    {
+        if (isCritical)
+        {
+            isCriticalBlinkTime += Time.deltaTime;
+            float colorAlpha = (Mathf.Sin(isCriticalBlinkTime * Mathf.PI * 2) + 1) * 0.5f;
+
+            var c = exclamationMarkImage.color;
+            exclamationMarkImage.color = new Color(c.r, c.g, c.b, colorAlpha);
+        }
     }
 
     private void UpdateTowerRectUIHeight(float height)
     {
-        var towerScale = towerRect.localScale;
-        towerScale.y = towerHeightDetector.TowerHeight / height;
-        towerRect.localScale = towerScale;
+        var towerOffsetMax = towerRect.offsetMax;
+        // here it's in pixed due to anchor
+        var heightRatio = 1 - (towerHeightDetector.TowerHeight / height);
+        var uiHeight = mask.rect.height;
+
+        towerOffsetMax.y = -heightRatio * uiHeight;
+        towerRect.offsetMax = towerOffsetMax;
     }
 
     private void UpdateWaterRectUIHeight(float height)
-    {
-        var waterScale = waterRect.localScale;
-        waterScale.y = floodLevel.FloodHeight / height;
-        waterRect.localScale = waterScale;
-    }
-
-    private void UpdateWaterRectUIHeight2(float height)
     {
         var waterOffsetMax = waterRect.offsetMax;
         // here it's in pixed due to anchor
@@ -64,6 +78,22 @@ public class FloodLevelHudController : MonoBehaviour
 
         playerAnchoredPosition.y = heightRatio * uiHeight;
         playerRect.anchoredPosition = playerAnchoredPosition;
+    }
+
+    private void UpdateIsCritical(float height)
+    {
+        var percent = floodLevel.FloodHeight / towerHeightDetector.TowerHeight;
+        if (percent > 0.6)
+        {
+            isCritical = true;
+        }
+        else
+        {
+            isCritical = false;
+            isCriticalBlinkTime = 0;
+            var c = exclamationMarkImage.color;
+            exclamationMarkImage.color = new Color(c.r, c.g, c.b, 0);
+        }
     }
 
     private float GetMaxGameHeight()
